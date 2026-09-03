4f8e7185bacad982030c852d224c7c57d18d3763
max <maks.skachkov.97@gmail.com>
2026-09-03 12:25:06 +0500

    Created all get handlers

--- main.go
+++ main.go
@@ -1,15 +1,24 @@
 package main
 
 import (
+	"errors"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
 	"time"
+	"uuid"
+
+	"github.com/google/uuid"
 )
 
 type Config struct {
 	Address string
+	Storage string
+}
+type Server struct {
+	Logger  *slog.Logger
 	Storage string
 }
 
@@ -32,13 +41,114 @@ func main() {
 		IdleTimeout:       60 * time.Second,
 		MaxHeaderBytes:    1 << 20,
 	}
+	//Создание Директорий
+	entityPath := filepath.Join(cfg.Storage, "entities")
+	evidencePath := filepath.Join(cfg.Storage, "evidence")
+	if err := os.MkdirAll(entityPath, 0755); err != nil {
+		logger.Error("problem creating entity dir")
+		return
+	}
+	if err := os.MkdirAll(evidencePath, 0755); err != nil {
+		logger.Error("problem creating evidence dir")
+		return
+	}
+	////////////////////
+	server := &Server{
+		Logger:  logger,
+		Storage: cfg.Storage,
+	}
+	//mux.HandleFunc("POST /api/v1/entities" , PostEntitiy)
+	mux.HandleFunc("GET /api/v1/entities/{id}", server.GetEntity)
+	mux.HandleFunc("GET /api/v1/evidence/{filename}", server.GetEvidence)
 
-	logger.Info("application started", slog.String("addres", cfg.Address), slog.Any("started_at ", time.Now()))
+	logger.Info("application started", slog.String("addr", cfg.Address), slog.Any("started_at ", time.Now()))
 
 	if err := srv.ListenAndServe(); err != nil {
-		logger.Error("Server wasnt able to launch", slog.String("error", err.Error()))
+		logger.Error("Server wasnt able to launch", slog.String("err", err.Error()))
 		return
 
 	}
 
+}
+
+func (s *Server) GetEntity(w http.ResponseWriter, r *http.Request) {
+	id := r.PathValue("id")
+
+	_, err := uuid.Parse(id)
+	if err != nil {
+		http.Error(w, "entity not found", http.StatusNotFound)
+		return
+	}
+
+	path := filepath.Join(s.Storage, "entities", id+".json")
+
+	data, err := os.ReadFile(path)
+	if err != nil {
+		s.Logger.Warn("unable to read file", slog.String("path", path), slog.Any("err", err))
+
+		if errors.Is(err, os.ErrNotExist) {
+			http.Error(w, "", http.StatusNotFound)
+			return
+		} else {
+			http.Error(w, "", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+
+	if _, err = w.Write(data); err != nil {
+		s.Logger.Error("unable to write file", slog.String("path", path), slog.Any("err", err))
+		return
+	}
+	return
+
+}
+
+func (s *Server) GetEvidence(w http.ResponseWriter, r *http.Request) {
+	filename := r.PathValue("filename")
+
+	base := filepath.Base(filename)
+
+	if filename != base || base == "" || base == "." || base == ".." {
+		s.Logger.Warn(
+			"user wrote wrong path",
+			slog.String("file_name", filename),
+		)
+
+		http.Error(w, "file not found", http.StatusNotFound)
+		return
+	}
+
+	path := filepath.Join(
+		s.Storage,
+		"evidence",
+		filename,
+	)
+
+	file, err := os.Open(path)
+	if err != nil {
+		s.Logger.Warn("wasn't able to open path", slog.String("path", path), slog.Any("err", err))
+		if errors.Is(err, os.ErrNotExist) {
+			http.Error(w, "", 404)
+			return
+		}
+		http.Error(w, "", 500)
+		return
+	}
+	defer file.Close()
+	info, err := file.Stat()
+	if err != nil {
+		s.Logger.Error("wasnt able to get info ", slog.String("file_name", filename), slog.Any("err", err))
+		http.Error(w, "", http.StatusInternalServerError)
+		return
+	}
+	if info.IsDir() {
+		s.Logger.Error("User tried to do dir traversal", slog.String("file_name", filename))
+		http.Error(w, "", http.StatusNotFound)
+		return
+	}
+
+	http.ServeContent(w, r, filename, info.ModTime(), file)
+	return
 }
